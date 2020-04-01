8f907ff03ffd32bdb1597eeaf6c3cf7c4129656b
cortocircuito96 <m.moroni0@gmail.com>
2020-04-02 00:54:57 +0200

    scheda sensore umidità forse da tarare

---
+++ SensorBoard_1.0/Debug/Core/Src/hts221_reg.d
@@ -0,0 +1,3 @@
+Core/Src/hts221_reg.o: ../Core/Src/hts221_reg.c ../Core/Src/hts221_reg.h
+
+../Core/Src/hts221_reg.h:
---
+++ SensorBoard_1.0/Debug/Core/Src/lps22hb_reg.d
@@ -0,0 +1,4 @@
+Core/Src/lps22hb_reg.o: ../Core/Src/lps22hb_reg.c \
+ ../Core/Src/lps22hb_reg.h
+
+../Core/Src/lps22hb_reg.h:
